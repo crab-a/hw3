5c7a8cda5ddb55b6e66ffb5a8e95e834ad666b5b
crab-a <crab@notmailinator.com>
2021-05-30 14:22:19 +0300

    made place for dist matrix and started with create samples

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import pandas
-
+import sample
 
 class Data:
 
@@ -11,30 +11,18 @@ class Data:
         df = pandas.read_csv(path)
         self.data = df.to_dict(orient="list")
 
-    def get_all_districts(self):
-        """
-        this method return a list of the regions in the data object
-        :return: list of the regions in the dataset
-        """
-        regions_set = set()
-        for region in self.data["denominazione_region"]:
-            regions_set.add(region)
-        return list(regions_set)
+    def create_samples(self):
+        samples = []
+        for index, sample_id in enumerate(self.data["samples"]):
+            s_id = self.data["s_id"][index]
+            genes = self.data["genes"][index]
+            label = self.data["type"][index]
+            new_sample = sample(s_id, genes, label)
 
-    def set_districts_data(self, districts):
+
+
+    def create_distance_matrix(self, samples):
+        self.distance_matrix[len(samples)][len(samples)] = 0
         """
-        this method receives a list of districts and change the data object keeping only the values for
-        the regions which are in that list
-        :param districts: a list of regions
-        :return: void
+        make distance matrix here, save it in self.distance_matrix
         """
-        new_data = {}
-        for key in self.data.keys():
-            new_col = []
-            data_entry = {}
-            for index, value in enumerate(self.data[key]):
-                if self.data['denominazione_region'][index] in districts:
-                    new_col.append(value)
-            data_entry[key] = new_col
-            new_data.update(data_entry)
-        self.data = new_data
---
+++ main.py
@@ -0,0 +1,9 @@
+import data
+
+
+def main(argv):
+    #  q1
+    print('Question 1:')
+
+    the_data_q1 = data.Data("Leukemia_sample.csv")
+    print(the_data_q1.data)
