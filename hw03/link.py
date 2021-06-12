ce62948865f7d473d224b18f50f59848f83f091e
crab-a <crab@notmailinator.com>
2021-06-12 22:25:03 +0300

    cluster.py made, need to check if work also need to get dominant label for print
    created link.py i have no idea how to make abstract method in python
    we need to move the distance matrix so it will be accssesable here..

---
+++ hw03/link.py
@@ -0,0 +1,7 @@
+class Link:
+    def compute(self, cluster, other):
+        pass
+
+class SingleLink:
+    def compute(self, cluster, other):
+        working_matrix = self.distance_matrix  # we need the distacne matrix here but its in data right now...
