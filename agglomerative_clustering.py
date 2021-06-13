fba7a422f8b8d9713f8fb603a0dc020e79027dc5
crab-a <crab@notmailinator.com>
2021-06-13 23:36:10 +0300

    נראלי אפשר להעביר את הטורי מרחקים מסאמפל לקלאסטר ואז אפשר בקלות לממש את מה שהצעתי במרחקים

--- agglomerative_clustering.py
+++ agglomerative_clustering.py
@@ -7,6 +7,7 @@ class AgglomerativeClustering:
         self.link = link
         self.samples = samples
         self.clusters = [cluster.Cluster(c_id, sample) for c_id, sample in enumerate(self.samples)]
+        self.distance_matrix = self.create_distance_matrix()
 
     def compute_silhouette(self):
         dict = {}
@@ -53,23 +54,43 @@ class AgglomerativeClustering:
 
         for i, sample1 in enumerate(self.samples):
             for j, sample2 in enumerate(self.samples):
-                if i > j:
-                    if sample1.label == sample2.label:
-                        FP += 1
-                        for current_cluster in self.clusters:
-                            if sample1 in current_cluster.samples & sample2 in current_cluster.samples:
-                                TP += 1
-                                FP -= 1
-                                break
-                    else:
-                        TN += 1
-                        for current_cluster in self.clusters:
-                            if sample1 in current_cluster.samples & sample2 in current_cluster.samples:
-                                FN += 1
-                                TN -= 1
-                                break
+                if i <= j:
+                    continue
+                if sample1.label == sample2.label:
+                    FP += 1
+                    for current_cluster in self.clusters:
+                        if sample1 in current_cluster.samples & sample2 in current_cluster.samples:
+                            TP += 1
+                            FP -= 1
+                            break
+                else:
+                    TN += 1
+                    for current_cluster in self.clusters:
+                        if sample1 in current_cluster.samples & sample2 in current_cluster.samples:
+                            FN += 1
+                            TN -= 1
+                            break
         true = TP + TN
         return true / sum([TP, TN, FP, FN])
 
+    def create_distance_matrix(self):
+        length = len(self.clusters)
+        distance_matrix = [[0] * length] * length
+        for this_cluster in self.clusters:
+            for other_cluster in self.clusters:
+                if this_cluster.id <= other_cluster.id:
+                    continue
+                distance_matrix[this_cluster.id][other_cluster.id] = self.link.compute(this_cluster, other_cluster)
+                distance_matrix[other_cluster.id][this_cluster.id] = distance_matrix[this_cluster.id][other_cluster.id]
+
+        return distance_matrix
+
     def run(self, max_clusters):
-        pass
+        new_clusters = []
+        length = len(self.clusters)
+        while length > max_clusters:
+            for current_cluster in self.clusters:  # need to uptade the dist matrix each time, need to prevent adding cluster to two diffrent clusters.. etc etc etc in short need more work!!
+                dist_to_closest_cluster = min(self.distance_matrix[current_cluster.id])
+                id_of_other_cluster = self.distance_matrix[current_cluster.id].index(dist_to_closest_cluster)
+                current_cluster.merge(self.clusters[id_of_other_cluster])
+                length -= 1
