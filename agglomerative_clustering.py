0a6dca56d983dc4d9f8591822f3ac2d844a47c71
crab-a <crab@notmailinator.com>
2021-06-13 20:38:33 +0300

    a

--- agglomerative_clustering.py
+++ agglomerative_clustering.py
@@ -1,2 +1,13 @@
 class AgglomerativeClustering:
+
     def __init__(self, link, samples):
+        self.link=link
+        self.samples=samples
+
+    def compute_rand_index(self):
+        TP = 0
+        TN = 0
+        FP = 0
+        FN = 0
+        for sample in samples:
+            do
